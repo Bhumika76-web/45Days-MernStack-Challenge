600ef02eabe13093ee617533b1aa4243a21c7e8d
Bhumika76-web <bhumikarajput0005@gmail.com>
2025-09-03 19:47:21 +0530

    Added Day 7: Node.js & Express - First Server

---
+++ Day 7/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/api', (req, res) => {
+  res.json({ message: "API is running!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
+
